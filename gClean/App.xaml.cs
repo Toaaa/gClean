4654101743a6bf9a2e3a9879f64d77783c989d3f
toaaa <toa@toaaa.de>
2023-10-02 12:10:44 +0200

    Revert "app delete"
    This reverts commit b64368a7ef9b3782e841f88b7aeef4bc7c818de2.

---
+++ gClean/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using KeyAuth;
+using System;
+using System.Windows;
+
+namespace gClean
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Login loginWindow = new Login();
+            loginWindow.Show();
+        }
+    }
+}
